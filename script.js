1ccffe517d8c4e79d9609d90138fcb511c566511
Coolbou0427 <58377572+Coolbou0427@users.noreply.github.com>
2023-09-26 10:26:07 -0600

    Stopped repeating
    made it not repeat until all cards have been shown

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let worksheet;
 let currentRow = 1;
 let currentCol = 0;
 let lastRow;
+let randomRows = [];
+let randomIndex = 0;
 
 const spreadsheet = document.getElementById('upload').addEventListener('change', handleFileSelect);
 const characters = document.getElementById('characters');
@@ -27,14 +29,18 @@ function handleFileSelect(event) {
         worksheet = workbook.Sheets[workbook.SheetNames[0]];
         const range = XLSX.utils.decode_range(worksheet['!ref']);
         lastRow = range.e.r + 1;
-        currentRow = getRandomInt(0, lastRow);
-        currentCol = 0;
+        
+        randomRows = [...Array(lastRow).keys()].slice(1); // Create an array [1, 2, ..., lastRow-1]
+        shuffleArray(randomRows); // Shuffle the array to get random order
+        randomIndex = 0; // Reset the index
+        currentRow = randomRows[randomIndex]; // Get the first random row
+        
         updateDisplay();
       };
       
       reader.readAsArrayBuffer(file);
     } else {
-        error.textContent = 'The file is not a .xlsx file';
+      error.textContent = 'The file is not a .xlsx file';
     }
   } else {
     error.textContent = 'No file selected';
@@ -60,22 +66,33 @@ function updateDisplay() {
   }
 }
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomInt() {
+  randomIndex += 1;
+  
+  if (randomIndex >= randomRows.length) {
+    shuffleArray(randomRows); // Reshuffle the array when all rows have been used
+    randomIndex = 0; // Reset the index
+  }
+  
+  return randomRows[randomIndex];
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 }
 
 document.addEventListener('keydown', function(event) {
-    if (event.code === 'Space') {
-      currentCol += 1;
-      
-      if (currentCol > 2) {
-        currentCol = 0;
-        currentRow = getRandomInt(1, lastRow);
-      }
-      
-      updateDisplay();
+  if (event.code === 'Space') {
+    currentCol += 1;
+    
+    if (currentCol > 2) {
+      currentCol = 0;
+      currentRow = getRandomInt(); // Get the next random row
     }
-  });
-  
+    
+    updateDisplay();
+  }
+});
